5a62a167be6cd6828370ef0d53e4c8d8e5a0c5f0
Vaibhav Bali <vbali@hub24.com.au>
2018-05-09 13:27:30 +1000

    completed lecture 4

---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,14 @@
+﻿(function() {
+    $.ready(function () {
+
+        
+
+        var $loginToggle = $("#loginToggle");
+        var $popupForm = $(".popup-form");
+
+        $loginToggle.on("click",
+            function() {
+                $popupForm.toggle(1000);
+            });
+    });
+}())
