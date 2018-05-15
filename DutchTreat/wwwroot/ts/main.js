b3a98c8adb4843b8b5730fb49edc0d953424f6bc
Vaibhav Bali <vbali@internode.on.net>
2018-05-15 21:55:44 +1000

    Added typescript support

---
+++ DutchTreat/wwwroot/ts/main.js
@@ -0,0 +1,5 @@
+"use strict";
+   var shopper = new StoreCustomer("Vaibhav", "Bali");
+   shopper.showName();
+
+//# sourceMappingURL=main.js.map
---
+++ DutchTreat/wwwroot/ts/storecustomer.ts
@@ -0,0 +1,22 @@
+﻿
+export class StoreCustomer {
+
+   public visits: number = 0;
+   private ourName: string;
+
+   constructor(private firstName: string, private lastName: string) {
+
+   }
+
+   public showName() {
+      alert(this.firstName + " " + this.lastName);
+   }
+
+   set name(val: string) {
+      this.ourName = val;
+   }
+
+   get name() : string {
+      return this.ourName;
+   }
+}
